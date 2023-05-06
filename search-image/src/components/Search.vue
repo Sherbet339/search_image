0a1f67415e0a1e9b672e310d5eac6663920e015a
Thamonwan <thamonwan@example.com>
2023-05-06 16:02:03 +0700

    add header and text field in Search.vue and add style scss

--- search-image/src/components/Search.vue
+++ search-image/src/components/Search.vue
@@ -1,9 +1,24 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col>
-                
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container class="search-component">
+    <h1>Unsplash</h1>
+    <v-text-field
+      v-model="search"
+      class="mt-6"
+      append-icon="mdi-magnify"
+      placeholder="Search for images"
+      outlined
+      hide-details
+      @keyup="filter()"
+    ></v-text-field>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: null,
+    };
+  },
+};
+</script>
